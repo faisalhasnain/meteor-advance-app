4aca5e9ee5fd978942f150008260f85b029853ad
Faisal Hasnain <faisal@faisalhasnain.com>
2018-01-20 19:24:08 +0500

    remvoed unused stuff from guest layout

--- imports/ui/layouts/GuestLayout.jsx
+++ imports/ui/layouts/GuestLayout.jsx
@@ -6,11 +6,7 @@ import './GuestLayout.css';
 export default props => (
   <section className="section">
     <div className="fluid-container guest-container">
-      <div className="app-title">
-        <img src="/logo.png" alt="WebDrvn" width="112" height="28" />
-        &nbsp;&nbsp;
-        <h1 className="subtitle is-3 has-text-primary">Snap</h1>
-      </div>
+      <h1 className="subtitle is-3 has-text-primary">Meteor Basic App</h1>
       <div className="space-block" />
       <div className="box">
         {props.page}
