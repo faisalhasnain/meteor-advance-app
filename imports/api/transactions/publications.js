6b9948ed2ee34a41575073ab3d6ef9732bd9f230
Faisal Hasnain <faisal@faisalhasnain.com>
2018-01-20 22:36:47 +0500

    fixed publication for transactions list

--- imports/api/transactions/publications.js
+++ imports/api/transactions/publications.js
@@ -2,6 +2,9 @@ import { Meteor } from 'meteor/meteor';
 
 import { Transactions } from './collection.js';
 
-Meteor.publish('transactions', function (query) {
-  return Transactions.find(query);
+Meteor.publish('Transactions.list', function () {
+  if (!this.userId) {
+    throw new Meteor.Error('accessDenied', 'You are not allowed to see this information');
+  }
+  return Transactions.find({}, { fields: { description: 1, type: 1, amount: 1, createdAt: 1 } });
 });
