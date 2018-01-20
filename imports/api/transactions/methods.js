bc35e01dc0f16894475333021553b3244cdda665
Faisal Hasnain <faisal@faisalhasnain.com>
2018-01-20 22:38:47 +0500

    added validation for transaction methods

--- imports/api/transactions/methods.js
+++ imports/api/transactions/methods.js
@@ -1,15 +1,21 @@
 import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
 
 import { Transactions } from './collection.js';
 
 Meteor.methods({
   'Transactions.insert'(transaction) {
+    check(transaction, Object);
+    if (!this.userId) {
+      throw new Meteor.Error('accessDenied', 'You are not allowed to perform this operation');
+    }
     return Transactions.insert(transaction);
   },
-  'Transactions.update'(query, modifier) {
-    return Transactions.update(query, modifier);
-  },
   'Transactions.remove'(id) {
+    check(id, String);
+    if (!this.userId) {
+      throw new Meteor.Error('accessDenied', 'You are not allowed to perform this operation');
+    }
     return Transactions.remove(id);
   }
 });
